1f9b570d1c731e58ccecd019c1b1c021b0367513
Maxime Haineault <haineault@gmail.com>
2017-01-16 00:46:04 -0500

    ..

--- idefix/static/idefix/js/idefix.js
+++ idefix/static/idefix/js/idefix.js
@@ -130,7 +130,15 @@ var BufferManager = Vue.component('buffer-manager', {
 
 var Fixture = Vue.component('fixture', {
   template: '#fixture',
-  props: ['k', 'v', 'data']
+  props: ['k', 'v', 'data', 'fields']
+})
+
+var ButtonAddFixture = Vue.component('add-fixture', {
+  template: '<span class="f-add fixture"><i class="fa fa-plus-square" aria-hidden="true"></i></span>',
+})
+
+var ButtonAddField = Vue.component('add-field', {
+  template: '<span class="f-add field"><i class="fa fa-plus-square" aria-hidden="true"></i></span>',
 })
 
 var components = {
